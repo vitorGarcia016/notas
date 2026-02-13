8a449f9c6e412661b7d7e584a6bed247c29412a7
vitorGarcia016 <vitorgarciaoliveira2005@gmail.com>
2026-02-13 16:58:37 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/main', [MainController::class , 'index']);
