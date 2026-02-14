502b6d8413d8e1fe7f73096fa20ecfab29f92535
João Vitor Garcia de Oliveira <vitorgarciaoliveira2005@gmail.com>
2026-02-14 17:03:05 -0300

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function index(){
-        return "Ola Controller";
+    public function index($nome){
+        return View("Main", [
+            'nome' => $nome
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/main', [MainController::class , 'index']);
+Route::get('/main/{nome}', [MainController::class, 'index']);
