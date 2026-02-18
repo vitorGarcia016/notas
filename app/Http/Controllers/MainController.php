dc8c8e78cbf02a73448a815fde64e32fae2efe4c
vitorGarcia016 <vitorgarciaoliveira2005@gmail.com>
2026-02-18 11:32:29 -0300

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,6 +7,31 @@ use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    
+
+    public function login(){
+        return view('login');
+    }
+
+    public function loginSubmit(Request $request){
+
+       $request -> validate(
+        [
+            "text_username" => "required",
+            "text_password" => "required"
+        ]
+       );
+
+       $userName = $request -> input("text_username");
+       $userPassword = $request -> input("text_password");
+
+       echo "ok";
+
+
+
+    }
+
+    public function logout(){
+        echo "logout";
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\MainController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-
+//Auth routes
+Route::get("/login", [MainController::class, 'login']);
+Route::post("/login_submit", [MainController::class, "loginSubmit"]);
+Route::get("/logout", [MainController::class, 'logout']);
