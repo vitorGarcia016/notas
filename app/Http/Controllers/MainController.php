36549215bdb102a386b0100179fda8817fa975c4
vitorGarcia016 <vitorgarciaoliveira2005@gmail.com>
2026-02-19 11:44:33 -0300

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Container\Attributes\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB as FacadesDB;
 use Illuminate\View\View;
+use PDOException;
+use PhpParser\Node\Stmt\Echo_;
+use PhpParser\Node\Stmt\TryCatch;
 
 class MainController extends Controller
 {
@@ -16,15 +21,30 @@ class MainController extends Controller
 
        $request -> validate(
         [
-            "text_username" => "required",
-            "text_password" => "required"
+            "text_username" => "required|email",
+            "text_password" => "required|min:4|max:10"
+        ],
+        [
+            "text_username.required" => "O campo username é obrigatório",
+            "text_username.email" => "Deve informar um email válido",
+            "text_password.required" => "O campo password é obrigatório",
+            "text_password.min" => "A password deve ter no minino :min caracteres",
+            "text_password.max" => "A password deve ter no maximo :max caracteres",
+
         ]
        );
 
        $userName = $request -> input("text_username");
        $userPassword = $request -> input("text_password");
 
-       echo "ok";
+       try {
+        FacadesDB::connection() -> getPdo();
+        echo "Sucesso ao tentar conectar ao bd";
+       } catch (\PDOException $th) {
+        echo "Falha ao tentar conectar: " .$th -> getMessage();
+       }
+
+       echo "FIM";
 
 
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,7 +18,7 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="text_username" class="form-label">Username</label>
-                                <input type="text" class="form-control bg-dark text-info" name="text_username" value="{{ old('text_username') }}">
+                                <input type="email" class="form-control bg-dark text-info" name="text_username" value="{{ old('text_username') }}" required>
                                 @error("text_username")
                                     <div class="text-danger">{{ $message }}</div>
 
@@ -27,7 +27,7 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="text_password" class="form-label">Password</label>
-                                <input type="password" class="form-control bg-dark text-info" name="text_password" value="{{ old('text_password') }}">
+                                <input type="password" class="form-control bg-dark text-info" name="text_password" value="{{ old('text_password') }}" required>
                                 @error("text_password")
                                     <div class="text-danger">{{ $message }}</div>
 
